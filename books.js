7bd8461a298f6a19ebfebf5ec920837c4fcc8aac
mkpgtr <mkpgtr@gmail.com>
2024-10-12 22:34:03 +0530

    add second entry to books

--- books.js
+++ books.js
@@ -3,5 +3,10 @@ export const books = [
         id : 1,
         title : "Ultimate Django for Web App Development Using Python: Build Modern, Reliable and Scalable Production-Grade Web Applications with Django and Python",
         coverImage : "https://images-eu.ssl-images-amazon.com/images/I/81LuSX049xL._AC_UL400_SR400,400_.jpg"
+    },
+    {
+        id : 2,
+        title : "whatever",
+        coverImage : null
     }
 ]
